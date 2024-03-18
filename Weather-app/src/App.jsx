671a76793aaea6236faf6f2c45cf61be8ca47497
lakshay1906 <lbhati020@gmail.com>
2024-03-18 18:33:57 +0530

    Weather app fixed

--- Weather-app/src/App.jsx
+++ Weather-app/src/App.jsx
@@ -3,42 +3,24 @@ import "./App.css";
 import Card from "./Components/Card_1";
 
 function App() {
-  const [btnText, setBtnText] = useState("Light Mode");
-  const [btnBg, setBtnBg] = useState("#fff");
-  let theme = false;
-  function themeValidation() {
-    if (theme) {
-      // document.getElementsByClassName("theme")[0].innerHTML = "Light Mode";
-      setBtnText("Light Mode");
-      document.body.style.backgroundColor = "#292929";
-      document.getElementsByClassName("theme")[0].style.backgroundColor =
-        "#fff";
-      setBtnBg("#fff");
-      document.getElementsByClassName("theme")[0].style.color = "black";
-      theme = false;
-    } else {
-      // document.getElementsByClassName("theme")[0].innerHTML = "Dark Mode";
-      setBtnText("Dark Mode");
-      document.body.style.backgroundColor = "#fff";
-      document.getElementsByClassName("theme")[0].style.backgroundColor =
-        "#000";
-      setBtnBg("#000");
-      document.getElementsByClassName("theme")[0].style.color = "white";
-      theme = true;
-    }
-  }
+  const [theme, setTheme] = useState(false);
+  const [bodyBg, setBodyBg] = useState("#292929");
+
+  document.body.style.backgroundColor = bodyBg;
+
   return (
     <>
       <button
         className="theme"
         onClick={(e) => {
-          themeValidation();
-          // e.target.innerHTML = btnText;
-          // e.target.style.backgroundColor = btnBg
-          console.log(e.target.innerHTML);
+          setTheme(!theme);
+          theme ? setBodyBg("#292929") : setBodyBg("#fff");
+          e.target.style.backgroundColor = theme ? "white" : "black";
+          e.target.style.color = theme ? "black" : "white";
+          e.target.innerText = theme ? "Light Mode" : "Dark Mode";
         }}
       >
-        {btnText}
+        Light Mode
       </button>
       <Card />
     </>
